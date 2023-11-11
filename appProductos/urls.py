3eb77c5234e49bdc7c99922a408ee1182e9b9569
marcoleonmora <mlmoram@misena.edu.co>
2023-11-11 11:32:17 -0500

    Segundos cambios de prueba

--- appProductos/urls.py
+++ appProductos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#Se agrega un comentario de prueba
+# Comentario para modificar el archivo
 urlpatterns = [
     path('producto/', views.verProductos, name="productos"),
     path('producto/<str:id>', views.verProductos, name="producto"), 
